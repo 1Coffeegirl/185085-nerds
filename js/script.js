ac6e666e0bb2e2d70d69480142944a3784762e63
Irina Tisserand <dogviolet@gmail.com>
2016-11-19 21:36:26 +0100

    1

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 function initMap() {
-	var mapDiv = document.getElementById('map');
-	var map = new google.maps.Map(mapDiv, {
-		center: {lat: 59.939090, lng: 30.319629},
-		zoom: 17
+  var mapDiv = document.getElementById("map");
+  var map = new google.maps.Map(mapDiv, {
+    center: {lat: 59.939090, lng: 30.319629},
+    zoom: 17
 });
-	var iconBase = 'img/marker.png';
-	var marker = new google.maps.Marker({
-  	position: {lat: 59.938607, lng: 30.323335},
-  	map: map,
-  	icon: iconBase
+  var iconBase = "img/marker.png";
+  var marker = new google.maps.Marker({
+    position: {lat: 59.938607, lng: 30.323335},
+    map: map,
+    icon: iconBase
 });
 }
 
@@ -17,9 +17,9 @@ var popup = document.querySelector(".modal-content");
 var close = document.querySelector(".modal-content-close");
 
 link.addEventListener ("click", function(event) {
-	event.preventDefault();
-	popup.classList.add("show");
+  event.preventDefault();
+  popup.classList.add("show");
 });
 close.addEventListener("click", function(event) {
-	popup.classList.remove("show");
+  popup.classList.remove("show");
 });
